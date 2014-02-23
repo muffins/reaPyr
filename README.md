7310d24f1f6e3fbb8e863c67c33e8c151bb00365
PoppySeedPlehzr <nanderson7@gmail.com>
2014-02-23 15:35:26 -0800

    Integrating pytsk3
    Added the barebones functionality of pytsk3, also updated the dependency
    listing in the README.md file.  Added TODO's in reapying.py

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ reapyng
 =======
 
 Project for Digital Forensics - CS6963
+
+
+Dependency listing:
+1.) pytsk3
+2.) 
--- reapying.py
+++ reapying.py
@@ -14,20 +14,63 @@
 #
 # TODO:
 #
-# 1.) Add harvesting for Windows OS
-#   a.) Windows Credentials
-#   b.) Browser Credentials
-#   c.) 3rd Party IMs (IRC, GTalk, Hangouts, etc...)
+#  1.) Automate the disk mount process
+#  2.) Harvest Windows/IE Credentials from windows reg
+#    a.) extract registry from tsk recovered files
+#  3.) Harvest FF/Chrome Credentials from Win FS
+#    a.) Locate where these DB's are
+#    b.) Read up on cracking whatever manner of protection exists
+#        on these dbs.
+#  4.) Add a logging function?
+#  5.) Hash out the reaper classes.
+#
+#
+#  Mount the FS with pytsk3. For the schardt disk image, this
+#  can be done with the following:
+#
+## Step 1: get an IMG_INFO object
+#img = pytsk3.Img_Info(url)
+#
+## Step 2: Open the filesystem
+#fs = pytsk3.FS_Info(img)
+#
+## Step 3: Open the directory node this will open the node based on path
+## or inode as specified.
+#directory = fs.open_dir(path=path, inode=inode)
+#
+## Step 4: Iterate over all files in the directory and print their
+## name. What you get in each iteration is a proxy object for the
+## TSK_FS_FILE struct - you can further dereference this struct into a 
+## TSK_FS_NAME and TSK_FS_META structs.
+#for f in directory:
+#        print f.info.meta.size, f.info.name.name
+#
+#
+#
+#
 
-import sys, os
+import sys, os, subprocess, pytsk3
 
 # Main handler. This should fire up classes of all reapers.
 def main(di):
-    pass
-
+    fs  = []
+    off = 0
+    img = pytsk3.Img_Info(di)
+    vol = pytsk3.Volume_Info(img)
+    # TODO:
+    # Automagically find the NTFS partion(s)
+    # and mount each one.
+    for ntfs_part in vol:
+        fs.append = pytsk3.FS_Info(part, offset=off) 
+    # To access the files
+    fs[0].open_dir(path=path)#, inode=inodei)
+    # see above for more information
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if sys.platform[:3] != 'lin':
+        print("reaPyng.py currently only supports linux.")
+        sys.exit()
+    elif len(sys.argv) != 2:
         print("Usage: %s <Disk Image>" % sys.argv[0])
         sys.exit()
     else:
