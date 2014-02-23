4247a3d2792e9d9fb312e3e7b52f905757b50701
PoppySeedPlehzr <nanderson7@gmail.com>
2014-02-23 08:57:36 -0800

    Initial push
    Adding reapying.py

---
+++ reapying.py
@@ -0,0 +1,17 @@
+
+# Program to harvest credentials from a given disk
+# image.
+#
+# TODO:
+#
+# 1.) Add harvesting for Windows OS
+#   a.) Windows Credentials
+#   b.) Browser Credentials
+#   c.) 3rd Party IMs (IRC, GTalk, Hangouts, etc...)
+
+
+
+
+
+if __name__ == "__main__":
+    pass
