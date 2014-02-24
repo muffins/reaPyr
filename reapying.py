d4a5143705830a9770b1b16559fccc73d70dfa03
PoppySeedPlehzr <nanderson7@gmail.com>
2014-02-24 12:12:58 -0500

    Adding placeholder _rpr.py files and updated reapying.py

--- reapying.py
+++ reapying.py
@@ -1,6 +1,16 @@
-
-# Program to harvest credentials from a given disk
-# image.
+# Nick Anderson 02/24/2014
+#
+# Digital Forensics, Spring 2014 - CS 6963 Final Project
+#
+# This program takes as input a Windows disk image, currently only dd
+# is supported, and outputs an enumerated list of credentials stored
+# on the image. 
+#
+# Each class of credential type is called a 'Reaper', and
+# is designed to target and harvest the specified credential
+# type specified in '<name>_rpr.py'
+# 
+#
 #
 # TODO:
 #
@@ -9,9 +19,21 @@
 #   b.) Browser Credentials
 #   c.) 3rd Party IMs (IRC, GTalk, Hangouts, etc...)
 
+import sys, os
 
-
+# Main handler. This should fire up classes of all reapers.
+def main(di):
+    pass
 
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) != 2:
+        print("Usage: %s <Disk Image>" % sys.argv[0])
+        sys.exit()
+    else:
+        # Parse the specified filename to ensure that it exists.
+        if os.path.isfile(sys.argv[1]):
+            main(sys.argv[1])
+        else:
+            print("Unable to open %s" % sys.argv[1])
+            sys.exit()
